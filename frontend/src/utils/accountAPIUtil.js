409d70b684d1cd73a9831a271d57e26286caf568
Samuel1337 <smbra33@gmail.com>
2022-07-20 16:06:51 -0700

    make adjustments for heroku deployment

--- frontend/src/utils/accountAPIUtil.js
+++ frontend/src/utils/accountAPIUtil.js
@@ -2,12 +2,15 @@ import axios from "axios";
 
 export const fetchTopDevnetAccounts = () => {
     console.log("Devnet request sent to Solana API. Waiting...")
-    return ( axios.get("/api/devnet"))
+    return (axios.get("/api/devnet"))
 }
 
 export const fetchTopMainnetAccounts = () => {
     console.log("Mainnet request sent to Solana API. Waiting...")
-    return (axios.get("/api/mainnet"))
+    return (
+        axios.get("/api/mainnet")
+        .catch(() => res.status(404).send({nodevnet: "Can't connect with the Mainnet cluster at the moment."}))
+    )
 }
 
 export const fetchTopTestnetAccounts = () => {
