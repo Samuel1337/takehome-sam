4f42d1a80b9a4a202afa64d67329a0a5aae0ee4b
Samuel1337 <smbra33@gmail.com>
2022-07-20 13:47:30 -0700

    make adjustments for heroku deployment

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 const axios = require("axios");
 const headers = require('./config/keys');
+const path = require("path");
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+   });
+}
 
 app.use( express.json() );
 
