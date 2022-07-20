75559a75e2ee414aa248b4aa8cc0749358eda978
Samuel1337 <smbra33@gmail.com>
2022-07-20 12:48:15 -0700

    adjust for heroku deployment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 const axios = require("axios");
-const { headers } = require('./config/keys');
+const headers = require('./config/keys');
 
 app.use( express.json() );
 
--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-export const headers = {
+module.exports = {
     "content-type": "application/json",
     "x-api-key": "1e146929-a84b-413b-b471-5e1e334cd1f0"
 }
